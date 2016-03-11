17d2aa220b66a32341c7208dee8d7b6f41f0f44f
Norhane <norhane.elgharbi@hotmail.fr>
2016-03-11 11:14:26 +0100

    bwt

--- src/bioseq.ml
+++ src/bioseq.ml
@@ -6,3 +6,18 @@ let main =
      in print_endline string
   | _ -> prerr_endline "commande inconue"
 	       
+let rec concat l1 l2 =
+	match (l1,l2) with
+		([],l2) -> []
+		|(l1,[]) -> []
+		|(x::r1,y::r2) -> (x^y) ::concat r1 l2
+
+
+let bwt chaine = 
+	let liste = List.map (Char.escaped) (String_mp.to_list chaine)
+	and result = liste
+	and n = List.length liste in
+	for i = 1 to n do
+		result = List.sort compare result;
+		result = concat liste result
+	done
